415dd379c5ead258ea9ce54a36c9b8da8982d9ae
Ziad Elsabbagh <ziad.maged@hotmail.com>
2025-05-14 15:55:37 +0300

    Modified the scene to place the character and the detective models

---
+++ Assets/Scripts/ModelPlacement.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ModelPlacement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    [Tooltip("This is the prefab of the character model we want to place in the scene.")]
+    private GameObject characterModelPrefab;
+    [SerializeField]
+    [Tooltip("This is the prefab of the detective model we want to place in the scene.")]
+    private GameObject detectiveModelPrefab;
+    private ARRaycastManager arRaycastManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private bool isDetectiveModelPlaced = false;
+    private bool isCharacterModelPlaced = false;
+
+    [SerializeField]
+    [Tooltip("This is the text on the canvas header to change the title.")]
+    private TMP_Text canvasHeaderText;
+    [SerializeField]
+    [Tooltip("This is the canvas.")]
+    private GameObject canvas;
+
+
+    private void Awake()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && !isCharacterModelPlaced && !isDetectiveModelPlaced)
+        {
+            //We place the character model when the user touches the screen
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (arRaycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = hits[0].pose;
+                    Instantiate(characterModelPrefab, hitPose.position, hitPose.rotation);
+                    isCharacterModelPlaced = true;
+                    //Change the canvas header text to "Detective"
+                    canvasHeaderText.text = "Place the Detective";
+                }
+            }
+        }
+        else if (Input.touchCount > 0 && isCharacterModelPlaced && !isDetectiveModelPlaced)
+        {
+            //We place the detective model when the user touches the screen
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (detectiveModelPrefab == null)
+                {
+                    canvasHeaderText.text = "Detective model not found... Yet!";
+                    return;
+                }
+                if (arRaycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = hits[0].pose;
+                    Instantiate(detectiveModelPrefab, hitPose.position, hitPose.rotation);
+                    isDetectiveModelPlaced = true;
+                    canvas.SetActive(false); // Hide the canvas after placing the detective model
+                    enabled = false; // Disable this script to prevent further placements
+                }
+            }
+        }
+    }
+}
