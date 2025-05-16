8fc243b40c24006d3a846880a335f2a8b695e99c
Ziad Elsabbagh <ziad.maged@hotmail.com>
2025-05-16 05:40:30 +0300

    Added other features to the scoreboard manager script

--- Assets/Scripts/ScoreboardManager.cs
+++ Assets/Scripts/ScoreboardManager.cs
@@ -7,17 +7,31 @@ public class ScoreboardManager : MonoBehaviour
     private int score;
 
     [SerializeField]
+    [Tooltip("This is the maximum number of hints that can be used.")]
+    private int maxHints;
+    private int currentHints;
+
+    [SerializeField]
+    [Tooltip("This is the maximum number of task skips that can be used.")]
+    private int maxNumberOfTaskSkips;
+    private int currentTaskSkipsUsed;
+
+    [SerializeField]
     [Tooltip("This is the text to display the score.")]
     private TMP_Text scoreText;
     void Start()
     {
         score = 0;
+        currentHints = maxHints;
+        currentTaskSkipsUsed = maxNumberOfTaskSkips;
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = $"Score: {score}";
+        // Hints.text = $"Hints Used: {currentHints}/{maxHints}";
+        // taskSkipsText.text = $"Task Skips Used: {currentTaskSkipsUsed}/{maxNumberOfTaskSkips}";
     }
 
     public void AddScore(int score)
@@ -33,5 +47,21 @@ public class ScoreboardManager : MonoBehaviour
     public void SetScore(int score)
     {
         this.score = score;
+    }
+
+    public void DecrementHintsUsed()
+    {
+        if (currentHints > 0)
+        {
+            currentHints--;
+        }
+    }
+
+    public void DecrementTaskSkips()
+    {
+        if (currentTaskSkipsUsed > 0)
+        {
+            currentTaskSkipsUsed--;
+        }
     }
 }
